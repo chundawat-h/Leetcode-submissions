763a78934e793ebe11837714c15dc82994b18bf6
Himanshu Kanwar Chundawat <himanshuchundawat00@gmail.com>
2026-06-21 20:41:24 +0530

    Time: 6 ms (12.64%) | Memory: 48.3 MB (5.26%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,27 +2,25 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         TreeSet<Integer> rows = new TreeSet<>();
         TreeSet<Integer> cols = new TreeSet<>();
-        for (int i = 0 ; i < matrix.length; i++){
-            for (int j = 0 ; j < matrix[i].length;j++){
-                if(matrix[i][j]==0){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
                     rows.add(i);
                     cols.add(j);
                 }
             }
         }
-        while(!rows.isEmpty()){
+        while (!rows.isEmpty()) {
             int x = rows.pollFirst();
-            for (int j = 0 ; j < matrix[0].length;j++){
-                matrix[x][j]=0;
-            }           
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[x][j] = 0;
+            }
         }
-        while(!cols.isEmpty()){
+        while (!cols.isEmpty()) {
             int x = cols.pollFirst();
-            for (int j = 0 ; j < matrix.length;j++){
-                matrix[j][x]=0;
-            }           
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[j][x] = 0;
+            }
         }
-
-
     }
 }
